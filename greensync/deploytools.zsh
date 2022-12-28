5047732d7fb17957d53338fcec523265ecadadc1
Brendan Weibrecht <brendan@weibrecht.net.au>
2022-12-29 08:15:20 +1100

    Add commented-out WIP deploytools functions from ages ago

--- greensync/deploytools.zsh
+++ greensync/deploytools.zsh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+# add-docker-rc() {
+#   local with_deploytools
+#   with_deploytools=$(grep -r -P '(?<=deploytools:)[^\{\$][^ "]+' -l)
+#   | xargs grep .docker-rc.d -L | xargs -r -t perl -i -pe 's%^( +)(-w /work \\)%\1\2\n\1-v ~/.docker-rc.d/:/.docker-rc.d/:ro \\%g'
+# }
+
+# add-docker-deploytools-home-cache() {
+#   grep -r -P '(?<=deploytools:)[^\{\$][^ "]+' -l | xargs grep deploytools-home-cache:/root -L | xargs -r -t perl -i -pe 's%^( +)(-w /work \\)%\1\2\n\1-v deploytools-home-cache:/root \\%g'
+# }
+
+
+
 add-docker-rc() {
   grep -r -P '(?<=deploytools:)[^\{\$][^ "]+' -l | xargs grep .docker-rc.d -L | xargs -r -t perl -i -pe 's%^( +)(-w /work \\)%\1\2\n\1-v ~/.docker-rc.d/:/.docker-rc.d/:ro \\%g'
 }
 
 # Makes sure my Docker dotfiles get loaded in non-interactive mode too
 wd() {
+  # add-docker-deploytools-volumes
   add-docker-rc
   ruby -r shellwords -e '
     if ARGV.length == 0
