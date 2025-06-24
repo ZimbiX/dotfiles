99170369420409c428e554d9e0dd153fd7989b76
Brendan Weibrecht <brendan@weibrecht.net.au>
2025-06-24 17:46:26 +1000

    Add Spring config

---
+++ ruby/spring.zsh
@@ -0,0 +1,6 @@
+# To avoid this error:
+# objc[16412]: +[__NSCFConstantString initialize] may have been in progress in another thread when fork() was called.
+# objc[16412]: +[__NSCFConstantString initialize] may have been in progress in another thread when fork() was called. We cannot safely call it or ignore it in the fork() child process. Crashing instead. Set a breakpoint on objc_initializeAfterForkError to debug.
+
+export PGGSSENCMODE="disable" # if this doesn't work, use "false"
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
