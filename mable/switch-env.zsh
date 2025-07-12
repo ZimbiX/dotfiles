9ce2577b3828ee7b3786cf5c1e3c8a9843cdb0e4
Brendan Weibrecht <brendan@weibrecht.net.au>
2025-07-13 03:38:38 +1000

    Make switch-env refocus Kitty after AWS SSO auto-login in Chrome

--- mable/switch-env.zsh
+++ mable/switch-env.zsh
@@ -1,3 +1,6 @@
 if [ -f "$PROJECTS/inf-scripts/switch-env/switch-env.sh" ]; then
-  . "$PROJECTS/inf-scripts/switch-env/switch-env.sh"
+  cat "$PROJECTS/inf-scripts/switch-env/switch-env.sh" \
+    | sed -E 's/(echo "Fetching auth details from AWS config:")/mac-focus-app kitty; \1/' \
+    > "$PROJECTS/inf-scripts/switch-env/switch-env-zimbix.sh"
+  . "$PROJECTS/inf-scripts/switch-env/switch-env-zimbix.sh"
 fi
