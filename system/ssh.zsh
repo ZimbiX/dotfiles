8e3ea2c1f73def92da0fa3a3b9da85239e2409ea
Brendan Weibrecht <brendan@weibrecht.net.au>
2025-08-24 02:45:59 +1000

    Convert ssh-init from script to function
    So I don't have to source ~/.ssh_agent myself after running it

--- system/ssh.zsh
+++ system/ssh.zsh
@@ -1,1 +1,8 @@
 [[ -f ~/.ssh_agent ]] && . ~/.ssh_agent >/dev/null
+
+# ssh-init is for systems where keys aren't loaded automatically, which is pretty much just Termux
+ssh-init() {
+  ssh-agent > ~/.ssh_agent
+  . ~/.ssh_agent
+  ssh-add
+}
