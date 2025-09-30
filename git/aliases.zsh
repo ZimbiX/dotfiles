1397aab29ef034956f28298db6b9938149e5e74d
Brendan Weibrecht <brendan@weibrecht.net.au>
2025-09-30 16:38:06 +1000

    ci alias: For GitHub Actions, open the run rather than one of the run's jobs

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -137,7 +137,7 @@ alias gra='git remote add'
 alias grr='git remote remove'
 
 alias cis='hub ci-status -v --color'
-alias ci='cis | tee /dev/stderr | grep -oE "https://\S+" | head -1 | xargs -r open &>/dev/null'
+alias ci='cis | tee /dev/stderr | grep -oE "https://\S+" | head -1 | sed -E "s=/job/[0-9]+\$==" | xargs -r open &>/dev/null'
 alias cir=ci-run
 
 ci-wait() {
