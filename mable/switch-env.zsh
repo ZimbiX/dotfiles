4a455208686becd890102e043c9be69c3735778f
Brendan Weibrecht <brendan@weibrecht.net.au>
2025-11-28 12:26:20 +1100

    Make switch-env log next command to run

--- mable/switch-env.zsh
+++ mable/switch-env.zsh
@@ -1,6 +1,9 @@
 if [ -f "$PROJECTS/inf-scripts/switch-env/switch-env.sh" ]; then
-  cat "$PROJECTS/inf-scripts/switch-env/switch-env.sh" \
+  {
+    cat "$PROJECTS/inf-scripts/switch-env/switch-env.sh" \
     | sed -E 's/(echo "Fetching auth details from AWS config:")/mac-focus-app kitty; \1/' \
-    > "$PROJECTS/inf-scripts/switch-env/switch-env-zimbix.sh"
+    | sed -E 's/^switch-env\(\) \{$/switch-env-orig() {/' &&
+    echo 'switch-env() { switch-env-orig "$@" && if [[ "$1" == "prd-lw" ]]; then printf "\n\e[32mNow run: eks-jumpbox-session\e[0m\n"; fi }'; \
+  } > "$PROJECTS/inf-scripts/switch-env/switch-env-zimbix.sh"
   . "$PROJECTS/inf-scripts/switch-env/switch-env-zimbix.sh"
 fi
