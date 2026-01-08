f4d649d88098f5f3f86aeb7668a12642d81609d6
Brendan Weibrecht <brendan@weibrecht.net.au>
2026-01-08 15:43:03 +1100

    Add switch-env aliases

--- mable/aliases.zsh
+++ mable/aliases.zsh
@@ -3,3 +3,6 @@ jira() {
   jira_issue_number="$(git rev-parse --abbrev-ref HEAD | grep -oE 'ES-\d+')"
   open "https://attainhealthtech.atlassian.net/browse/$jira_issue_number"
 }
+
+alias ses='log_and_run_command switch-env stg'
+alias sep='log_and_run_command switch-env prd-lw'
