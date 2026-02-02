4f887e4476428f4c82b1882ddbca97b1efde3724
Brendan Weibrecht <brendan@weibrecht.net.au>
2026-02-02 22:59:14 +1100

    Update switch-env customisation for the addition of prod-access
    I've been using this for a while and it works great

--- mable/switch-env.zsh
+++ mable/switch-env.zsh
@@ -1,11 +1,31 @@
-if [ -f "$PROJECTS/inf-scripts/switch-env/switch-env.sh" ]; then
-  {
-    cat "$PROJECTS/inf-scripts/switch-env/switch-env.sh" \
-    | sed -E 's/(echo "Fetching auth details from AWS config:")/mac-focus-app kitty; \1/' \
-    | sed -E 's/^switch-env\(\) \{$/switch-env-orig() {/' && \
-    cat << EOF
+if [ -f "$PROJECTS/inf-scripts/util/aws-creds" ]; then
+  # New version using `prod-access`
+
+  cat "$PROJECTS/inf-scripts/util/aws-creds" \
+  | sed -E 's/(^.*Fetching auth details from AWS config)/mac-focus-app kitty; \1/' \
+  > "$PROJECTS/inf-scripts/util/aws-creds-zimbix"
+  chmod +x "$PROJECTS/inf-scripts/util/aws-creds-zimbix"
+
+  cat "$PROJECTS/inf-scripts/prod-access/prod-access.sh" \
+  | sed -E 's/aws-creds/aws-creds-zimbix/g' \
+  > "$PROJECTS/inf-scripts/prod-access/prod-access-zimbix.sh"
+  source "$PROJECTS/inf-scripts/prod-access/prod-access-zimbix.sh"
+
+  cat "$PROJECTS/inf-scripts/switch-env/switch-env.sh" \
+  | sed -E 's/aws-creds/aws-creds-zimbix/g' \
+  > "$PROJECTS/inf-scripts/switch-env/switch-env-zimbix.sh"
+  source "$PROJECTS/inf-scripts/switch-env/switch-env-zimbix.sh"
+else
+  # Old version
+
+  if [ -f "$PROJECTS/inf-scripts/switch-env/switch-env.sh" ]; then
+    {
+      cat "$PROJECTS/inf-scripts/switch-env/switch-env.sh" \
+      | sed -E 's/(echo "Fetching auth details from AWS config:")/mac-focus-app kitty; \1/' \
+      | sed -E 's/^switch-env\(\) \{$/switch-env-orig() {/' && \
+      cat << EOF
 switch-env() {
-  switch-env-orig "\$@"
+  switch-env-orig "\$@" || return 1
   if [[ "\$1" == prd* ]]; then
     printf "\\n\\e[32mNow run: eks-jumpbox-session\\e[0m\\n"
   else
@@ -13,6 +33,7 @@ switch-env() {
   fi
 }
 EOF
-  } > "$PROJECTS/inf-scripts/switch-env/switch-env-zimbix.sh"
-  . "$PROJECTS/inf-scripts/switch-env/switch-env-zimbix.sh"
+    } > "$PROJECTS/inf-scripts/switch-env/switch-env-zimbix.sh"
+    . "$PROJECTS/inf-scripts/switch-env/switch-env-zimbix.sh"
+  fi
 fi
