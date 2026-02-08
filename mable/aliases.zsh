95356ddaaf6232915e9e496604447af252da0d3a
Brendan Weibrecht <brendan@weibrecht.net.au>
2026-02-08 17:34:24 +1100

    Add Mable aliases for linting Ruby in better_caring

--- mable/aliases.zsh
+++ mable/aliases.zsh
@@ -1,2 +1,5 @@
 alias ses='log_and_run_command switch-env stg'
 alias sep='log_and_run_command switch-env prd-lw'
+
+alias jl='log_and_run_command just lint'
+alias jlf='log_and_run_command just lint-fix'
