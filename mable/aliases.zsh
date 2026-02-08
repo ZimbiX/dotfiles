cc5e3cdef58fe33c66cb77a35a553a6503de7503
Brendan Weibrecht <brendan@weibrecht.net.au>
2026-02-08 17:33:37 +1100

    Remove jira function now that I'm trying jira-cli

--- mable/aliases.zsh
+++ mable/aliases.zsh
@@ -1,8 +1,2 @@
-jira() {
-  local jira_issue_number
-  jira_issue_number="$(git rev-parse --abbrev-ref HEAD | grep -oE 'ES-\d+')"
-  open "https://attainhealthtech.atlassian.net/browse/$jira_issue_number"
-}
-
 alias ses='log_and_run_command switch-env stg'
 alias sep='log_and_run_command switch-env prd-lw'
